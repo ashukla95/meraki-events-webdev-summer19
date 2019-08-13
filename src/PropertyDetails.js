2ca6c32866b3974862066100b16cad59bbd5ef92
ashukla95 <ashukla95@gmail.com>
2019-08-12 20:53:07 -0400

    Meraki Rating Integrated.

--- src/PropertyDetails.js
+++ src/PropertyDetails.js
@@ -1,39 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import TextSearchAPIService from './APIServices/TextSearchAPIService'
 import withRoot from "./modules/withRoot";
 import SearchBar from "./modules/views/SearchBar";
 import DetailsCard from "./modules/components/DetailsCard";
-import { Grid } from "@material-ui/core";
+import {Grid} from "@material-ui/core";
+import MerakiReviewSearch from "./APIServices/MerakiReviewSearch";
 
 class PropertyDetails extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { placeDetails: {} };
-        TextSearchAPIService.getInstance()
-            .getPlaceDetails(props.match.params.placeId)
-            .then(placeDetails => {
-                this.setState({ placeDetails });
-            })
-    }
+	constructor(props) {
+		super(props);
+		console.log("place id received: ", props.match.params.placeId);
+		this.state = {placeDetails: {}, merakiRating: 0};
+		TextSearchAPIService.getInstance()
+			.getPlaceDetails(props.match.params.placeId)
+			.then(placeDetails => {
+				this.setState({placeDetails});
+			});
+		MerakiReviewSearch.getInstance()
+			.getPlaceReview(props.match.params.placeId)
+			.then(response => {
+				this.setState({
+					...this.state,
+					merakiRating: response.rating
+				});
+			})
+			.catch(error => true);
+	}
 
 
-    render() {
-        return (
-            <div>
-                <div>
-                    <SearchBar />
-                </div>
-                <div>
-                    <Grid item lg={12} md={12} sm={12}>
-                        <DetailsCard
-                            result={this.state.placeDetails}
-                            imageHeight={365}
-                            showLearnMore={true} />
-                    </Grid>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<div>
+					<SearchBar/>
+				</div>
+				<div>
+					<Grid item lg={12} md={12} sm={12}>
+						<DetailsCard
+							merakiRating={this.state.merakiRating}
+							result={this.state.placeDetails}
+							imageHeight={365}
+							showLearnMore={true}/>
+					</Grid>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default withRoot(PropertyDetails);
--- src/modules/components/DetailsCard.js
+++ src/modules/components/DetailsCard.js
@@ -9,79 +9,114 @@ import Button from "@material-ui/core/Button";
 import {Link} from 'react-router-dom';
 import Grid from "@material-ui/core/Grid";
 import Rating from 'react-rating'
+import Box from "@material-ui/core/Box";
 
 const styleCustom = makeStyles({
-    /*Set the css property for the card to be rendered after retrieval of the data from the back-end service.*/
-    card: {
-        margin: 10,
-        padding: 10,
-    },
-    link: {
-        textDecoration: "none",
-        color: "black",
-    }
+	/*Set the css property for the card to be rendered after retrieval of the data from the back-end service.*/
+	card: {
+		margin: 10,
+		padding: 10,
+	},
+	link: {
+		textDecoration: "none",
+		color: "black",
+	}
 });
 
 /*Stateless component that renders the card with the information obtained from the parent component.*/
 const DetailsCard = ({
-                         result,
-                         imageHeight,
-                         showLearnMore
+	                     result,
+	                     imageHeight,
+	                     showLearnMore,
+	                     merakiRating
 
                      }) => {
-    const classes = styleCustom();
-    return (
-        <Card raised
-              margin={2}
-              className={classes.card}>
-            <Link className={classes.link} to={`/property/${result.place_id}/`}>
-                <CardActionArea>
-                    <CardMedia
-                        height={imageHeight}
-                        component="img"
-                        alt={result["name"]}
-                        image="https://picsum.photos/id/870/500/500?"
-                        title={result["name"]}/>
-                    <CardContent>
-                        <Typography variant={"body1"} component={"p"}>
-                            {result["name"]}
-                        </Typography>
-                        <Grid container justify={"space-between"}>
-                            <Grid item>
-                                <Typography gutterBottom
-                                            variant="body1"
-                                            component="p">
-                                    {result["description"] ? result["description"] : result["formatted_address"]}
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                {showLearnMore &&
-                                <Rating initialRating={result["rating"]}
-                                        readonly
-                                        stop={5}
-                                        emptySymbol={['fa fa-star-o  red']}
-                                        fullSymbol={['fa fa-star  red']}/>}
-                            </Grid>
-                        </Grid>
-                    </CardContent>
-                </CardActionArea>
-            </Link>
-            <CardActions>
-                <Button size="small"
-                        color="primary" href={`/book-now/`}>
-                    Book Now
-                </Button>
-                {!showLearnMore &&
-                <Link className={classes.link} to={`/property/${result.place_id}/`}>
-                    <Button size="small"
-                            color="primary">
-                        Learn More
-                    </Button>
-                </Link>}
+	console.log("result: ", result);
+	const classes = styleCustom();
+	return (
+		<Card raised
+		      margin={2}
+		      className={classes.card}>
+			<Link className={classes.link} to={`/property/${result.place_id}/`}>
+				<CardActionArea>
+					<CardMedia
+						height={imageHeight}
+						component="img"
+						alt={result["name"]}
+						image="https://picsum.photos/id/870/500/500?"
+						title={result["name"]}/>
+					<CardContent>
+						<Grid container justify={"space-between"}>
+							<Grid item>
+								<Typography variant={"body1"} component={"p"}>
+									{result["name"]}
+								</Typography>
+							</Grid>
 
-            </CardActions>
-        </Card>
-    );
+							<Grid item>
+								<Grid container spacing={1} direction={"row"} justify={"space-between"}>
+									<Grid item>
+										<Typography variant={"body1"} component={"p"}>
+											Meraki Rating:
+										</Typography>
+									</Grid>
+									<Grid item>
+										<Rating initialRating={merakiRating}
+										        readonly
+										        stop={5}
+										        emptySymbol={['fa fa-star-o  red']}
+										        fullSymbol={['fa fa-star  red']}/>
+									</Grid>
+								</Grid>
+							</Grid>
+						</Grid>
+						<Grid container justify={"space-between"}>
+							<Grid item>
+								<Typography gutterBottom
+								            variant="body1"
+								            component="p">
+									{result["description"] ? result["description"] : result["formatted_address"]}
+								</Typography>
+							</Grid>
+							<Grid item>
+								{
+									showLearnMore &&
+									<Grid container spacing={1} direction={"row"} justify={"space-between"}>
+										<Grid item>
+											<Typography variant={"body1"} component={"p"}>
+												Google Rating:
+											</Typography>
+										</Grid>
+										<Grid item>
+											<Rating initialRating={result["rating"]}
+											        readonly
+											        stop={5}
+											        emptySymbol={['fa fa-star-o  red']}
+											        fullSymbol={['fa fa-star  red']}/>
+										</Grid>
+									</Grid>
+								}
+							</Grid>
+						</Grid>
+					</CardContent>
+				</CardActionArea>
+			</Link>
+			<CardActions>
+				<Button size="small"
+				        color="primary" href={`/book-now/`}>
+					Book Now
+				</Button>
+				{!showLearnMore &&
+				<Link className={classes.link} to={`/property/${result.place_id}/`}>
+					<Button size="small"
+					        color="primary">
+						Learn More
+					</Button>
+				</Link>}
+
+			</CardActions>
+		</Card>
+	);
 };
 
 export default DetailsCard;
